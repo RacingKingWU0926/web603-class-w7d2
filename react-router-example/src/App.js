a25f51022b8ce0f0c5e85a1997d7caf06c626dec
JasonLei <jlei@activision.com>
2024-02-22 14:13:52 -0800

    add Nav

--- react-router-example/src/App.js
+++ react-router-example/src/App.js
@@ -1,14 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
-  )
-}
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function Home() {
   return (
@@ -17,6 +7,31 @@ function Home() {
       <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
     </div>
   );
+}
+
+function About() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>About View</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <nav style={{ margin: 10 }}>
+        <Link to="/" style={{ padding: 5 }}>Home</Link>
+        <Link to="/about" style={{ padding: 5 }}>About</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App;
